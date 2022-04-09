b80b24d20f9b328e7353c1205427fb4a55570ad8
Nick Visconti <naviscont@coastal.edu>
2022-04-08 21:33:12 -0400

    Added SQL

--- Repository/MovieRepository.cs
+++ Repository/MovieRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System;
 using MovieApi.Models;
+using MySql.Data.MySqlClient;
 
 namespace MovieApi.Repository {
     public class MovieRepository : IMovieRepository {
@@ -10,44 +12,87 @@ namespace MovieApi.Repository {
             new Movie {Name="The Godfather", Genre="Crime", Year=1972}
         };
 
+        private MySqlConnection _connection;
         public MovieRepository() {
+            string connectionString="server=localhost;userid=csci330user;password=csci330pass;database=entertainment";
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }   
 
+        ~MovieRepository() {
+            _connection.Close();
         }
 
         public IEnumerable<Movie> GetAll() {
-            return movies;
+            var statement = "Select * from Movies";
+            var command = new MySqlCommand(statement, _connection);
+            var results = command.ExecuteReader();
+
+            List<Movie> newList = new List<Movie>(20);
+
+            while(results.Read()) {
+                Movie m = new Movie {
+                    Name = (string) results[1],
+                    Genre = (string) results[3],
+                    Year = (int) results[2]
+                };
+
+                newList.Add(m);
+                
+            }
+            results.Close();
+            return newList;
         }
 
         public Movie GetMovieByName(string name) {
-            foreach(Movie m in movies){
-                if(m.Name.Equals(name)){
-                    return m;
-                }
-            }
-            return null;
+            var statement = "Select * from Movies where Name = @newName";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@newName", name);
+
+            var results = command.ExecuteReader();
+            Movie m = null;
+            
+            if(results.Read()) {
+                    m = new Movie {
+                    Name = (string) results[1],
+                    Genre = (string) results[3],
+                    Year = (int) results[2]
+                };
+            } 
+            results.Close();
+            return m;
         }
 
         public void InsertMovie(Movie m) {
-            movies.Add(m);
+            var statement = "Insert into Movies (Name, Year, Genre) Values(@n, @y, @g)";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@n", m.Name);
+            command.Parameters.AddWithValue("@y", m.Year);
+            command.Parameters.AddWithValue("@g", m.Genre);
+
+            int results = command.ExecuteNonQuery();
+            Console.WriteLine(results);
+            
+            
         }
 
         public void UpdateMovie(string name, Movie movieIn) {
-            foreach(Movie m in movies){
-                if(m.Name.Equals(name)){
-                    m.Name = movieIn.Name;
-                    m.Genre = movieIn.Genre;
-                    m.Year = movieIn.Year;
-                }
-            }
+            var statement = "Update Movies Set Name=@newName, Year=@newYear, Genre=@newGenre Where Name=@updateName";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@newName", movieIn.Name);
+            command.Parameters.AddWithValue("@newYear", movieIn.Year);
+            command.Parameters.AddWithValue("@newGenre", movieIn.Genre);
+            command.Parameters.AddWithValue("updateName", name);
+
+            int results = command.ExecuteNonQuery();
         }
 
         public void DeleteMovie(string name) {
-            foreach(Movie m in movies){
-                if(m.Name.Equals(name)){
-                    movies.Remove(m);
-                    return;
-                }
-            }
+            var statement = "Delete from Movies where Name = @delName";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@delName", name);
+
+            int results = command.ExecuteNonQuery();
         }
     }
 }
